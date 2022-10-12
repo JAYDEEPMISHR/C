fae76ca1503f060d4fd5c38c02afa9eaf955a9d6
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2022-10-12 23:42:22 +0530

    intriduction to c

---
+++ INTRODUCTION.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	printf("NAME :\t\tJAYDEEP MISHRA");
+	printf("\nCONTACT NUMBER:\t8140903191");
+	printf("\nADDRESS:\tD-160 TEJENDRANAGAR PART-7 \n \t\tTRAGAD ROAD \n \t\tCHANDKHEDA");
+	printf("\nEMAIL ID:\tjaydeepmishra1996@gmail.com");
+	printf("\nDATE OF BIRTH:\t11 APR 1996");
+	printf("\nQUALIFICATION:\tM.E(EPS)");
+	printf("\nAGE:\t\t26");
+}
