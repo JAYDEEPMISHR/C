4bfaede554cd46b9fd3375f443ef2fd48b9d9b19
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2022-10-17 22:22:53 +0530

    Multiplication

---
+++ multiplicationwithscanf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int num1, num2;
+	int ans;
+	printf("\n ENTER VALUE NUMBER 1= ");
+	scanf("%d",&num1);
+	
+	printf("\n ENTER VALUE NUMBER 2= ");
+	scanf("%d",&num2);
+	
+	printf("\n NUMBER 1= %d",num1);
+	printf("\n NUMBER 2= %d",num2);
+	
+	ans= num1*num2;
+	printf("\n ANSWER= %d",ans);	
+}
