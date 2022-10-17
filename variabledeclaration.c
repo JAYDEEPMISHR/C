18101701a3691a2599f4f8eee66ee4f112ba9f0a
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2022-10-17 22:23:01 +0530

    Create variabledeclaration.c

---
+++ variabledeclaration.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int num1;
+	int num2;
+	int ans;
+	
+	num1=50;
+	num2=20;
+	
+	printf("\n Value of Num1= %d",num1);
+	printf("\n Value of Num2= %d",num2);
+	
+	ans= num1-num2;
+	printf("\n Answer= %d",ans);
+}
