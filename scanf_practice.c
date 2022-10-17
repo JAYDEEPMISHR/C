f2fdeda9794f6238dcff54505c9ba1eb0cca2f80
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2022-10-17 22:44:59 +0530

    scan_practice

---
+++ scanf_practice.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	int num1;
+	int num2;
+	int Ans;
+	
+	printf("\nNumber1=%d",num1);
+	scanf("%d",&num1);
+	
+	printf("\nNumber2=%d",num2);
+	scanf("%d",&num2);
+	
+	printf("\nValue of Number=%d", num1);
+	printf("\nValue of Number=%d", num2);
+	
+	Ans=num1-num2;
+	printf("\nAnswer=%d",Ans);
+}
