f5a6be516f7fd49337df9eb485047c2d096a6557
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2022-10-20 22:29:24 +0530

    userdefinedtask

---
+++ squareofnumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a,ans;
+	
+	printf("Value of a=");
+	scanf("%d",&a);
+	printf("\nGiven number is= %d",a);
+	ans=a*a;
+	printf("\nSquare of given number is %d",ans);
+}
