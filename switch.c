455af28edca00a44a55ee24fb07f98616069bfa0
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2022-11-02 17:41:46 +0530

    switch

---
+++ switch.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+main()
+{
+	int days;
+	printf("ENTER NUMBER :");
+	scanf("%d",&days);
+	
+	switch(days)
+	{
+		case 1:
+		printf("SUNDAY");
+		break;
+		
+		case 2:
+		printf("MONDAY");
+		break;
+		
+		case 3:
+		printf("TUESDAY");
+		break;
+		
+		case 4:
+		printf("WEDNESDAY");
+		break;
+		
+		case 5:
+		printf("THURSDAY");
+		break;
+		
+		case 6:
+		printf("FRIDAY");
+		break;
+		
+		case 7:
+		printf("SATURDAY");
+		break;
+		
+		default:
+			printf("SOMETHING IS WRONG");
+	}
+}
